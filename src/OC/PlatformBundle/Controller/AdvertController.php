8d737758e83baf7a5045f7dbc025470f8d5d1dd2
Guillaume Benoit <gbenoit79@gmail.com>
2014-11-15 16:26:29 +0100

    The Symfony2 router

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -8,12 +8,29 @@ class AdvertController extends Controller
 {
     public function indexAction()
     {
-        $content = $this
-            ->get('templating')
-            ->render('OCPlatformBundle:Advert:index.html.twig', array(
-                'nom' => 'winzou'
-            )
+        // On veut avoir l'URL de l'annonce d'id 5.
+        $url = $this->get('router')->generate(
+            'oc_platform_view', // 1er argument : le nom de la route
+            array('id' => 5)    // 2e argument : les valeurs des paramètres
         );
-    	return new Response($content);
+        // $url vaut « /platform/advert/5 »
+
+        return new Response("L'URL de l'annonce d'id 5 est : ".$url);
+    }
+
+    // La route fait appel à OCPlatformBundle:Advert:view,
+    // on doit donc définir la méthode viewAction.
+    // On donne à cette méthode l'argument $id, pour
+    // correspondre au paramètre {id} de la route
+    public function viewAction($id)
+    {
+        // $id vaut 5 si l'on a appelé l'URL /platform/advert/5
+
+        // Ici, on récupèrera depuis la base de données
+        // l'annonce correspondant à l'id $id.
+        // Puis on passera l'annonce à la vue pour
+        // qu'elle puisse l'afficher
+
+        return new Response("Affichage de l'annonce d'id : ".$id);
     }
 }
