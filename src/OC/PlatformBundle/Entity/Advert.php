1d5d6ca7da36f724553fab75f8ba20284f3a5394
Guillaume Benoit <gbenoit79@gmail.com>
2014-11-23 17:29:05 +0100

    Manipulate entities with Doctrine

--- src/OC/PlatformBundle/Entity/Advert.php
+++ src/OC/PlatformBundle/Entity/Advert.php
@@ -49,6 +49,18 @@ class Advert
      */
     private $content;
 
+    /**
+     * @ORM\Column(name="published", type="boolean")
+     */
+    private $published = true;
+
+
+    public function __construct()
+    {
+        // Par défaut, la date de l'annonce est la date d'aujourd'hui
+        $this->date = new \Datetime();
+    }
+
 
     /**
      * Get id
@@ -150,5 +162,28 @@ class Advert
     public function getContent()
     {
         return $this->content;
+    }
+
+    /**
+     * Set published
+     *
+     * @param boolean $published
+     * @return Advert
+     */
+    public function setPublished($published)
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+
+    /**
+     * Get published
+     *
+     * @return boolean 
+     */
+    public function getPublished()
+    {
+        return $this->published;
     }
 }
