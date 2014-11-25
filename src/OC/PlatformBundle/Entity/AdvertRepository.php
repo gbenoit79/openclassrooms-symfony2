b8d5823e46319eef021ba5eacebae5b35795ddf6
Guillaume BENOIT <gbenoit79@gmail.com>
2014-11-25 18:10:05 +0100

    Get entities with Doctrine

--- src/OC/PlatformBundle/Entity/AdvertRepository.php
+++ src/OC/PlatformBundle/Entity/AdvertRepository.php
@@ -12,4 +12,24 @@ use Doctrine\ORM\EntityRepository;
  */
 class AdvertRepository extends EntityRepository
 {
+    public function getAdvertWithCategories(array $categoryNames)
+    {
+        $qb = $this->createQueryBuilder('a');
+
+        // On fait une jointure avec l'entité Category avec pour alias « c »
+        $qb
+            ->join('a.categories', 'c')
+            ->addSelect('c')
+        ;
+
+        // Puis on filtre sur le nom des catégories à l'aide d'un IN
+        $qb->where($qb->expr()->in('c.name', $categoryNames));
+        // La syntaxe du IN et d'autres expressions se trouve dans la documentation Doctrine
+
+        // Enfin, on retourne le résultat
+        return $qb
+            ->getQuery()
+            ->getResult()
+        ;
+    }
 }
--- src/OC/PlatformBundle/Entity/ApplicationRepository.php
+++ src/OC/PlatformBundle/Entity/ApplicationRepository.php
@@ -12,4 +12,23 @@ use Doctrine\ORM\EntityRepository;
  */
 class ApplicationRepository extends EntityRepository
 {
+    public function getApplicationsWithAdvert($limit)
+    {
+        $qb = $this->createQueryBuilder('a');
+
+        // On fait une jointure avec l'entité Advert avec pour alias « adv »
+        $qb
+            ->join('a.advert', 'adv')
+            ->addSelect('adv')
+        ;
+
+        // Puis on ne retourne que $limit résultats
+        $qb->setMaxResults($limit);
+
+        // Enfin, on retourne le résultat
+        return $qb
+            ->getQuery()
+            ->getResult()
+        ;
+    }
 }
